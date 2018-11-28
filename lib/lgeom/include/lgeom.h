5a3d13c1f0a5c3f44e8f0bed3fd53f4fd2f0d389
cody <cody.m.lee@live.mercer.edu>
2018-11-28 08:05:41 -0500

    Removed unneccessary code from library

--- lib/lgeom/include/lgeom.h
+++ lib/lgeom/include/lgeom.h
@@ -1,14 +1,9 @@
 #ifndef LGEOM_H
 #define LGEOM_H
 
-#include "lgeom/clipping.h"
-#include "lgeom/intersect.h"
 #include "lgeom/mat.h"
 #include "lgeom/matfuncs.h"
 #include "lgeom/misc.h"
-#include "lgeom/polyutil.h" // Avery Zebell - zgeom
-#include "lgeom/quat.h" // Avery Zebell - zgeom
-#include "lgeom/tessellation.h"
 #include "lgeom/vec.h"
 
 #endif // LGEOM_H
