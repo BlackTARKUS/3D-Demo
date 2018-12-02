106c9e463fb7a04bba26d25ef08adbba506ba662
cody <cody.m.lee@live.mercer.edu>
2018-12-02 12:04:56 -0500

    house finalized

--- src/house.cpp
+++ src/house.cpp
@@ -2,48 +2,67 @@
 #include "glFuncs.h"
 #include <stdlib.h>
 
+/* The house is stored as a long series of sequential points in 3D space
+ * which are then assembled into polygonal faces by the render function
+ * colors are also assigned by the render function.
+*/
 House::House() {
+	// <--------- House Faces ---------> //
 	// Bottom Face
-	this->V.push_back( {-1.0, -1.0, 0.0} );
-	this->V.push_back( {-1.0,  1.0, 0.0} );
-	this->V.push_back( { 1.0,  1.0, 0.0} );
-	this->V.push_back( { 1.0, -1.0, 0.0} );
-
-	// Left Face
-	this->V.push_back( {-1.0, -1.0, 0.0} );
-	this->V.push_back( {-1.0, -1.0, 1.0} );
-	this->V.push_back( { 1.0, -1.0, 1.0} );
-	this->V.push_back( { 1.0, -1.0, 0.0} );
-
-	// Right Face
-	this->V.push_back( {-1.0,  1.0, 0.0} );
-	this->V.push_back( {-1.0,  1.0, 1.0} );
-	this->V.push_back( { 1.0,  1.0, 1.0} );
-	this->V.push_back( { 1.0,  1.0, 0.0} );
-
+	this->V.push_back( {-1.0, 0.0, 0.0} );
+	this->V.push_back( {-1.0, 0.0, 1.0} );
+	this->V.push_back( { 1.0, 0.0, 1.0} );
+	this->V.push_back( { 1.0, 0.0, 0.0} );
 	// Back Face
-	this->V.push_back( {-1.0, -1.0, 0.0} );
-	this->V.push_back( {-1.0, -1.0, 1.0} );
-	this->V.push_back( {-1.0,  1.0, 1.0} );
-	this->V.push_back( {-1.0,  1.0, 0.0} );
-
+	this->V.push_back( {-1.0, 0.0, 0.0} );
+	this->V.push_back( {-1.0, 1.0, 0.0} );
+	this->V.push_back( { 1.0, 1.0, 0.0} );
+	this->V.push_back( { 1.0, 0.0, 0.0} );
+	// Left Face
+	this->V.push_back( {-1.0, 0.0, 0.0} );
+	this->V.push_back( {-1.0, 0.0, 1.0} );
+	this->V.push_back( {-1.0, 1.0, 1.0} );
+	this->V.push_back( {-1.0, 1.0, 0.0} );
+	// Right Face
+	this->V.push_back( { 1.0, 0.0, 0.0} );
+	this->V.push_back( { 1.0, 0.0, 1.0} );
+	this->V.push_back( { 1.0, 1.0, 1.0} );
+	this->V.push_back( { 1.0, 1.0, 0.0} );
 	// Front Face
-	this->V.push_back( { 1.0, -1.0, 0.0} );
-	this->V.push_back( { 1.0, -1.0, 1.0} );
-	this->V.push_back( { 1.0,  1.0, 1.0} );
-	this->V.push_back( { 1.0,  1.0, 0.0} );
-
+	this->V.push_back( { 1.0, 1.0, 1.0} );
+	this->V.push_back( {-1.0, 1.0, 1.0} );
+	this->V.push_back( {-1.0, 0.0, 1.0} );
+	this->V.push_back( { 1.0, 0.0, 1.0} );
 	// Top Face
-	this->V.push_back( { 1.0,  1.0, 1.0} );
-	this->V.push_back( {-1.0,  1.0, 1.0} );
-	this->V.push_back( {-1.0, -1.0, 1.0} );
-	this->V.push_back( { 1.0, -1.0, 1.0} );
-
-	// Roof Front Face
-	this->V.push_back( { 1.0, -1.0, 0.0} );
-	this->V.push_back( { 1.0, -1.0, 1.0} );
-	this->V.push_back( { 1.0,  1.0, 1.0} );
-	this->V.push_back( { 1.0,  1.0, 0.0} );
+	this->V.push_back( {-1.0, 1.0, 0.0} );
+	this->V.push_back( {-1.0, 1.0, 1.0} );
+	this->V.push_back( { 1.0, 1.0, 1.0} );
+	this->V.push_back( { 1.0, 1.0, 0.0} );
+	// <--------- Roof  Faces ---------> //
+	// Top Left Face
+	this->V.push_back( {-1.5, 1.0, 0.25} );
+	this->V.push_back( {-1.5, 1.0, 1.25} );
+	this->V.push_back( { 0.0, 1.3, 1.25} );
+	this->V.push_back( { 0.0, 1.3, 0.25} );
+	// Top Right Face
+	this->V.push_back( { 0.0, 1.3, 0.25} );
+	this->V.push_back( { 0.0, 1.3, 1.25} );
+	this->V.push_back( { 1.5, 1.0, 1.25} );
+	this->V.push_back( { 1.5, 1.0, 0.25} );
+	// Roof Base Face
+	this->V.push_back( {-1.5, 1.0, 0.25} );
+	this->V.push_back( {-1.5, 1.0, 1.25} );
+	this->V.push_back( { 1.5, 1.0, 1.25} );
+	this->V.push_back( { 1.5, 1.0, 0.25} );
+	// <------- Triangle  Time! -------> //
+	// Front Triangle
+	this->V.push_back( { 0.0, 1.3, 1.25} );
+	this->V.push_back( {-1.5, 1.0, 1.25} );
+	this->V.push_back( { 1.5, 1.0, 1.25} );
+	// back Triangle
+	this->V.push_back( { 0.0, 1.3, 0.25} );
+	this->V.push_back( {-1.5, 1.0, 0.25} );
+	this->V.push_back( { 1.5, 1.0, 0.25} );
 }
 
 // rotate House with new rotation direction
@@ -54,6 +73,10 @@ void House::rotate(float theta,float x,float y,float z) {
 	glPopMatrix();
 }
 
+/* Stores a ton of predefined colours that I have specified ahead of time
+ * colo function choses each of these sequentially per face so I could clean
+ * the house's positional data up
+*/
 void House::colo(int i, int &c1, int &c2, int &c3){
 	switch(i){
 		case 0:
@@ -80,6 +103,15 @@ void House::colo(int i, int &c1, int &c2, int &c3){
 		case 28:
 			c1 = 102; c2 = 51; c3 = 0;
 			break;
+		case 32:
+			c1 = 88; c2 = 69; c3 = 40;
+			break;
+		case 36:
+			c1 = 255; c2 = 255; c3 = 255;
+			break;
+		case 39:
+			c1 = 88; c2 = 88; c3 = 88;
+			break;
 	}
 }
 
@@ -90,14 +122,25 @@ void House::render() {
 	glPolygonMode(GL_FRONT, GL_FILL);
 	glPolygonMode(GL_BACK, GL_FILL);
 
-	for(unsigned i=0;i<this->V.size();i+=4) {
+	// Draw the faces made of squares
+	for(unsigned i=0;i<36;i+=4) {
 		colo(i,c1,c2,c3);
 		glColor3ub(c1,c2,c3);
 		glBegin(GL_POLYGON);
-			glVertex3i(this->V[i].x,this->V[i].y,this->V[i].z);
-			glVertex3i(this->V[i+1].x,this->V[i+1].y,this->V[i+1].z);
-			glVertex3i(this->V[i+2].x,this->V[i+2].y,this->V[i+2].z);
-			glVertex3i(this->V[i+3].x,this->V[i+3].y,this->V[i+3].z);
+			glVertex3f(this->V[i].x,this->V[i].y,this->V[i].z);
+			glVertex3f(this->V[i+1].x,this->V[i+1].y,this->V[i+1].z);
+			glVertex3f(this->V[i+2].x,this->V[i+2].y,this->V[i+2].z);
+			glVertex3f(this->V[i+3].x,this->V[i+3].y,this->V[i+3].z);
+		glEnd();
+	}
+	// Draw the faces made of triangles
+	for(unsigned i=36;i<this->V.size();i+=3) {
+		colo(i,c1,c2,c3);
+		glColor3ub(c1,c2,c3);
+		glBegin(GL_POLYGON);
+			glVertex3f(this->V[i].x,this->V[i].y,this->V[i].z);
+			glVertex3f(this->V[i+1].x,this->V[i+1].y,this->V[i+1].z);
+			glVertex3f(this->V[i+2].x,this->V[i+2].y,this->V[i+2].z);
 		glEnd();
 	}
 }
