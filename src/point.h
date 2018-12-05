ed08b2093a49c63c5a838a3e9e92a2df1f2831ae
Tarkus <cody.m.lee@live.mercer.edu>
2018-12-05 11:19:02 -0500

    fixed a typo

--- src/point.h
+++ src/point.h
@@ -1,7 +1,7 @@
 #ifndef POINT_H
 #define POINT_H
 
-typedef struct point { // Defines a 3D pointtor as 3 floats
+typedef struct point { // Defines a 3D point as 3 floats
 	float x;
 	float y;
 	float z;
